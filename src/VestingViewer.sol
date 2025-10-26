cc1b645fef2bfb68b1f8fd65119c15ac118603a8
Xiaozhou Li <xl@cs.princeton.edu>
2025-10-26 16:08:21 -0700

    update viewer

--- src/VestingViewer.sol
+++ src/VestingViewer.sol
@@ -29,9 +29,40 @@ contract VestingViewer {
             releasedAmounts[i] = vestingContract.released(b);
         }
     }
+
+    function getVestingCompleteInfo(address[] calldata _beneficiaries)
+        external
+        view
+        returns (
+            uint256[] memory allocations,
+            uint256[] memory vestedAmounts,
+            uint256[] memory releasedAmounts,
+            uint256[] memory releasableAmounts
+        )
+    {
+        uint256 n = _beneficiaries.length;
+        allocations = new uint256[](n);
+        vestedAmounts = new uint256[](n);
+        releasedAmounts = new uint256[](n);
+        releasableAmounts = new uint256[](n);
+
+        for (uint256 i = 0; i < n; ++i) {
+            address b = _beneficiaries[i];
+            uint256 alloc = vestingContract.allocations(b);
+            uint256 vested = vestingContract.vestingSchedule(b);
+            uint256 rel = vestingContract.released(b);
+
+            allocations[i] = alloc;
+            vestedAmounts[i] = vested;
+            releasedAmounts[i] = rel;
+            // Compute releasable as vested - released to avoid requiring an extra interface method
+            releasableAmounts[i] = vested - rel;
+        }
+    }
 }
 
 interface ITokenVesting {
     function vestingSchedule(address _beneficiary) external view returns (uint256);
     function released(address _beneficiary) external view returns (uint256);
+    function allocations(address _beneficiary) external view returns (uint256);
 }
